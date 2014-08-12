65df4cf7b8a9f7acca25a260ae2252fb5cdd316b
Suresh Nelamangala <suresh.avadhani@gmail.com>
2014-08-12 22:53:08 +0530

    mods to fruitYeild method

--- orangeTree.rb
+++ orangeTree.rb
@@ -1,9 +1,9 @@
 class OrangeTree
 
     def initialize
-        @height = 1
+        @height = 0
         @treeAge = 0
-        @fruiteYeild = 10
+        @fruitYeild = 10
     end
 
     def height
@@ -14,20 +14,20 @@ class OrangeTree
 
         if @height < 15
             @height = @height.to_i + 1
-            @fruiteYeild = 0
             puts @height.to_s + " is the age of the tree"
         else
             puts "The tree has to die , sorry !"
         end
     end
 
-    def fruitYeild
+    def fruitYeilds
 
-        if @height < 3
+        if @height <= 3
             puts "Tree too young to bear fruit"
         else
             if @height > 3 && @height < 15
-            @fruitYeild = @fruiteYeild.to_i + 10
+            @fruitYeild = @fruitYeild.to_i + 10
+            puts @fruitYeild.to_s + " are to be harvested"
             else
                 puts "Tree is too old and will die , no fruits"
             end
@@ -37,5 +37,34 @@ class OrangeTree
     end
 
 
- puts OrangeTree.new.oneYearPasses
+    ot = OrangeTree.new
+    ot.oneYearPasses
+    ot.fruitYeilds
+    ot.oneYearPasses
+    ot.oneYearPasses
+    ot.fruitYeilds
+    ot.oneYearPasses
+    ot.oneYearPasses
+ot.fruitYeilds
+    ot.oneYearPasses
+ot.fruitYeilds
+    ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+ot.oneYearPasses
+    ot.fruitYeilds
+
 
